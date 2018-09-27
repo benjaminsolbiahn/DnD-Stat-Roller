957ad6b6b4ee912ab67a334de4497dd4dc242c98
Benjamin Ahn <43593958+benjaminsolbiahn@users.noreply.github.com>
2018-09-27 16:11:15 -0700

    Update 4d6-lowest.py

--- 4d6-lowest.py
+++ 4d6-lowest.py
@@ -1,11 +1,17 @@
 import random
 
-for x in range(6):
-    a = random.randint(1,6)
-    b = random.randint(1,6)
-    c = random.randint(1,6)
-    d = random.randint(1,6)
-    print(a, b, c, d)
-    total = a + b + c + d - min(a, b, c, d)
-    print(total)
+print('If you want to roll for stats, tell me "roll"')
 
+command = input()
+
+if command == str('roll'):
+    for x in range(6):
+        a = random.randint(1,6)
+        b = random.randint(1,6)
+        c = random.randint(1,6)
+        d = random.randint(1,6)
+        print(a, b, c, d)
+        total = a + b + c + d - min(a, b, c, d)
+        print(total)
+else:
+    print('That isn\'t "roll"!')
